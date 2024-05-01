3c5bdcc617e90bce2e5b4663f50343cfa05044c9
Jesus <jesusrosas.dm@gmail.com>
2024-04-30 18:40:41 -0600

    Update test ProjectManagement

--- test/ProjectManagementTest.js
+++ test/ProjectManagementTest.js
@@ -24,6 +24,8 @@ contract("ProjectManagement", (accounts) => {
         await instance.assignStudentToProject(0, student1, {from: company});
         await instance.assignStudentToProject(0, student2, {from: company});
         await instance.assignStudentToProject(0, student3, {from: company});
+
+       
         const project = await instance.getProject(0);
         assert.equal(project.assignedTo.length, 3, "There should be three students assigned to the project");
     });
@@ -33,18 +35,51 @@ contract("ProjectManagement", (accounts) => {
         //await instance.assignFundsToProject(0, web3.utils.toWei("3", "ether"), {from: admin});
 
 
-        await instance.assignFundsToProject(0, web3.utils.toWei("3", "ether"), {
+        await instance.assignFundsToProject(0, web3.utils.toWei("9", "ether"), {
             from: admin,
-            value: web3.utils.toWei("3", "ether")
+            value: web3.utils.toWei("9", "ether")
         });
 
+        
         //pruebas anteriores dejaron el proyecto como completado y no pasaba la distribucion de fondos se
         //agrega manualmente el estatus Active antes para asegurar (solo para pruebas)
         //await instance.changeProjectStatus(0, "Active", {from: company});
+
+        // Get initial balances of students
+        const initialBalance1 = await web3.eth.getBalance(student1);
+        const initialBalance2 = await web3.eth.getBalance(student2);
+        const initialBalance3 = await web3.eth.getBalance(student3);
+
+        console.log(`Initial balances: 
+                    Student1: ${web3.utils.fromWei(initialBalance1, 'ether')} ETH,
+                    Student2: ${web3.utils.fromWei(initialBalance2, 'ether')} ETH,
+                    Student3: ${web3.utils.fromWei(initialBalance3, 'ether')} ETH`);
+
+
+
         
         await instance.completeAndDistributeFunds(0, {from: company});
-        const balance = await web3.eth.getBalance(student1);
-        assert(balance > web3.utils.toWei("0.99", "ether"), "Student should receive their share of the funds");
+
+
+        // Get final balances of students
+        const finalBalance1 = await web3.eth.getBalance(student1);
+        const finalBalance2 = await web3.eth.getBalance(student2);
+        const finalBalance3 = await web3.eth.getBalance(student3);
+
+        console.log(`Final balances: 
+                    Student1: ${web3.utils.fromWei(finalBalance1, 'ether')} ETH,
+                    Student2: ${web3.utils.fromWei(finalBalance2, 'ether')} ETH,
+                    Student3: ${web3.utils.fromWei(finalBalance3, 'ether')} ETH`);
+
+        // Check if the balances increased as expected
+        assert(finalBalance1 > initialBalance1, "Student 1 did not receive funds correctly.");
+        assert(finalBalance2 > initialBalance2, "Student 2 did not receive funds correctly.");
+        assert(finalBalance3 > initialBalance3, "Student 3 did not receive funds correctly.");
+
+
+
+        // const balance = await web3.eth.getBalance(student1);
+        // assert(balance > web3.utils.toWei("0.99", "ether"), "Student should receive their share of the funds");
         
     });
 });
