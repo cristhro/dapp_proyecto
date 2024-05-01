94d693981fdb97583957b43b436b2f708ce9e3e4
Cristhian <cristhianrodriguez.dev@gmail.com>
2024-05-02 00:35:07 +0200

    Rollback setup host

--- scripts/createUsers.js
+++ scripts/createUsers.js
@@ -14,21 +14,21 @@ module.exports = async function (callback) {
   const userManagmentContract = await UserManagment.deployed();
 
   const adminUser = {
-    address: '0xB351f49D330D5FEDEfE6b8AFD7a5Cf49527deF8F',
+    address: '0xB351f49D330D5FEDEfE6b8AFD7a5Cf49527deF8F', // TODO: change this address
     name: 'admin',
     email: 'admin@test.com',
     tipoUsuario: 4, // 1 = student, 2 = company, 3 = instructor, 4 = admin
     imageURI: 'https://robohash.org/b6ffbeafcbfe8bf535e1fbb159a51f4a?set=set4&bgset=&size=400x400'
   }
   const studentUser = {
-    address: '0xEdB89341560Fc1c9f45c2bb7Ea05954DfcD22934',
+    address: '0xEdB89341560Fc1c9f45c2bb7Ea05954DfcD22934', // TODO: change this address
     name: 'student',
     email: 'student@test.com',
     tipoUsuario: 1, // 1 = student, 2 = company, 3 = instructor, 4 = admin
     imageURI: 'https://gravatar.com/avatar/b6ffbeafcbfe8bf535e1fbb159a51f4a?s=400&d=robohash&r=x'
   }
   const companyUser = {
-    address: '0x523Ed327544A48615f89e35A19EE149be1A95CDd',
+    address: '0x523Ed327544A48615f89e35A19EE149be1A95CDd', // TODO: change this address
     name: 'company',
     email: 'company@test.com',
     tipoUsuario: 2, // 1 = student, 2 = company, 3 = instructor, 4 = admin
